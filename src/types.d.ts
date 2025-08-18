014b76630132ecbfe9957821ed00c81d950550b8
John Paul <johnpaulviado20@gmail.com>
2025-08-18 11:35:27 +0800

    feat: add interfaces for video processing and requests

---
+++ src/types.d.ts
@@ -0,0 +1,35 @@
+import { Request } from "express";
+
+export interface VideoInfo {
+  title: string;
+  duration: string;
+  thumbnail?: string | undefined;
+  author: string;
+  viewCount: string;
+  description?: string | undefined;
+}
+
+export interface ConversionData {
+  status: "processing" | "completed" | "failed";
+  title: string;
+  filename: string;
+  progress: number;
+  createdAt: Date;
+  completedAt?: Date;
+  error?: string;
+}
+
+export interface VideoInfoRequest extends Request {
+  body: {
+    url: string;
+  };
+}
+
+export interface ConvertRequest extends Request {
+  body: {
+    url: string;
+    bitrate?: BitRateOptions;
+  };
+}
+
+type BitRateOptions = 64 | 128 | 192 | 256 | 320;
